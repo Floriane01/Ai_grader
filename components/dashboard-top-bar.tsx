0cfcf80d28e5a21cad7b158599d8bdfe1fb24b60
Daril DJODJO KOUTON <109483193+DarilDivin@users.noreply.github.com>
2025-03-10 14:35:14 +0100

    Ajout de la border

--- components/dashboard-top-bar.tsx
+++ components/dashboard-top-bar.tsx
@@ -19,7 +19,7 @@ import DarkModeToggle from "./dark-mode-toggle";
 
 const DashboardTopBar = ({ name }: { name: string }) => {
   return (
-    <div className="flex h-14 w-full bg-sidebar text-sidebar-foreground justify-between pr-6 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <div className="flex h-14 w-full bg-sidebar text-sidebar-foreground justify-between pr-6 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b border-sidebar-border">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1 text-sidebar-foreground" />
         <h3 className="text-sidebar-foreground text-xl">Hii, {name}</h3>
